6c0f856fa1289d23bf599dd2fd8b454b33b02841
aniqazmi <s10189235@connect.np.edu.sg>
2019-10-29 16:32:57 +0800

    Initial Commit

---
+++ MAD2_Week3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAD2_Week3
+//
+//  Created by MAD2_P01 on 29/10/19.
+//  Copyright © 2019 MAD2_P01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
